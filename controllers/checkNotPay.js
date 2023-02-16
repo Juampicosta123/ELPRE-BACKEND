5e25de6fb3d93de16ff714a2197eb463b98f7fa9
Juampicosta123 <juampicosta81@gmail.com>
2023-02-16 20:20:17 -0300

    notpay

--- controllers/checkNotPay.js
+++ controllers/checkNotPay.js
@@ -1,15 +1,18 @@
-const  studentsModel  = require("../models/students");
+const studentsModel = require("../models/students");
 const { handleHttpError } = require("../utils/handleError");
 const express = require("express");
 const { IncomingWebhook } = require("@slack/webhook");
-const webHook = new IncomingWebhook("https://hooks.slack.com/services/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");
 const morganBody = require("morgan-body");
+const webHook = new IncomingWebhook(
+  "https://hooks.slack.com/services/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"
+);
 const app = express();
 const checkNotPay = async (req, res) => {
   try {
+    
     let year = new Date().getFullYear();
     let month = new Date().getMonth() + 1;
-    let day = new Date().getDate()
+    let day = new Date().getDate();
     const students = await studentsModel.find({});
     const alumns = students
       .filter((student) => student?.pay.length !== 0)
@@ -19,9 +22,9 @@ const checkNotPay = async (req, res) => {
       .filter(
         (student) => student?.pay[student?.pay.length - 1]?.month < `${month}`
       );
-      if(day >= 15){
-        for (i = 0; i <= alumns?.length; i++) {
-          if(alumns[i] !== undefined)
+    if (day >= 15) {
+      for (i = 0; i <= alumns?.length; i++) {
+        if (alumns[i] !== undefined)
           morganBody(app, {
             stream: {
               write: webHook.send({
@@ -29,12 +32,11 @@ const checkNotPay = async (req, res) => {
               }),
             },
           });
-        }
-        res.send("listo")
-      } else {
-        res.send("no se puede")
       }
-    
+      res.send("listo");
+    } else {
+      res.send("no se puede");
+    }
   } catch (e) {
     handleHttpError(res, e.message);
   }
